c1a7624dba35976338d6b3e70bf5177b4579d3c6
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-05-21 17:41:31 +0400

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+
+
+class Calendar {
+    constructor(url, api_key) {
+        this.url = url;
+        this.api_key = api_key;
+        this.state = {}
+    }
+
+    //https://calendarific.com/api/v2/holidays?&api_key=ae861d1de3c14a0f8922e45f84bdae1dde18e9a0&country=US&year=2020
+
+
+    search(keyword) {
+        let url = `${this.url}?&api_key=${this.api_key}&country=${keyword}&year=2020`;
+        
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+        
+                    data=data.response.holidays;
+
+                    data.forEach(element => { 
+                        if (element.locations == "All") {
+                        document.querySelector("#main").innerHTML += render_data_to_dom(element)
+                    }
+            
+        });
+            })
+    }
+}
+render_data_to_dom = (data) => {
+    return `
+        <div class="col-md-6">
+        <p><strong>Name :</strong> <small class = "holiday_name">${data.name}</small></p>
+        <p><strong>Description :</strong> <small class = "desc">${data.description}</small></p>
+        <p><strong>Country :</strong> <small class = "country">${data.country.name}</small></p>
+        <p><strong>Date :</strong> <small class = "date">${data.date.datetime.day} . ${data.date.datetime.month} . ${data.date.datetime.year}</small></p>
+        <p><strong>Type :</strong> <small class = "holiday_type">${data.type}</small></p>
+        <hr>
+        </div> `;
+}
+

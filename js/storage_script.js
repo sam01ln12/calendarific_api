2c4704ebfc09679500719e760132c84f0c487027
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-05-22 17:39:54 +0400

    Update storage_script.js

--- js/storage_script.js
+++ js/storage_script.js
@@ -1,3 +1,23 @@
-function calendar_storage (keyword, data) {
-    window.localStorage.setItem(keyword, JSON.stringify(data));
+calendar_storage = (keyword, data)=> { 
+    let holidays = calendar_get();
+    holidays.push(JSON.stringify({country:keyword, data:data}));
+    window.localStorage.setItem("id", holidays);
+
 }
+
+
+calendar_get =  () => {
+
+    let holidays = localStorage.getItem("id");
+
+    if(holidays == undefined) {
+        holidays = [];
+    } 
+    else {
+    holidays = holidays.split(",");
+    }
+    return holidays;
+
+    //document.querySelector(".info-row").innerHTML += render_data_to_table(holidays);
+}
+

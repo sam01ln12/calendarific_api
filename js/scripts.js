6e0112a78d1962edaf8c40d074c32700925ff2a1
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-05-28 12:41:14 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -33,15 +33,23 @@ class Calendar {
     
 }
 render_data_to_dom = (data) => {
-    return `
-        <div class="col-md-6">
-        <p><strong>Name : </strong> <small class = "holiday_name">${data.name}</small></p>
-        <p><strong>Description : </strong> <small class = "desc">${data.description}</small></p>
-        <p><strong>Country : </strong> <small class = "country">${data.country.name}</small></p>
-        <p><strong>Date : </strong> <small class = "date">${data.date.datetime.day} . ${data.date.datetime.month} . ${data.date.datetime.year}</small></p>
-        <p><strong>Type : </strong> <small class = "holiday_type">${data.type}</small></p>
-        <hr>
-        </div> `;
+return ` <div class="col-md-6">
+<div class="card mb-3">
+  <h3 class="card-header">Holiday</h3>
+  <div class="card-body">
+    <h5 class="card-title">${data.name}</h5>
+    
+  </div>
+  <img style="height: 200px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image">
+  <div class="card-body">
+    <p class="card-text">Description: ${data.description}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Type: ${data.type}</li>
+    <li class="list-group-item">Date: ${data.date.datetime.day} . ${data.date.datetime.month} . ${data.date.datetime.year}</li>
+  </ul>
+</div>
+</div>`;
 }
 
 render_data_to_table = (data, index) => {

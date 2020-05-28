a641fdf6bd5372c9696683d2d61cca811cacf125
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-05-28 12:55:41 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ class Calendar {
     constructor(url, api_key) {
         this.url = url;
         this.api_key = api_key;
-        this.state = {}
     }
 
     //https://calendarific.com/api/v2/holidays?&api_key=ae861d1de3c14a0f8922e45f84bdae1dde18e9a0&country=US&year=2019
@@ -18,6 +17,8 @@ class Calendar {
             .then(data => {
         
                     data=data.response.holidays;
+                    document.querySelector("#main").innerHTML = "";
+
 
                     data.forEach(element => { 
                         if (element.locations == "All") {
@@ -32,6 +33,7 @@ class Calendar {
     }
     
 }
+
 render_data_to_dom = (data) => {
 return ` <div class="col-md-6">
 <div class="card mb-3">
@@ -40,12 +42,13 @@ return ` <div class="col-md-6">
     <h5 class="card-title">${data.name}</h5>
     
   </div>
-  <img style="height: 200px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image">
+  <img style="height: 200px; width: 100%; display: block;" src="https://www.worldometers.info/img/flags/${data.country.id}-flag.gif" alt="Card image">
   <div class="card-body">
     <p class="card-text">Description: ${data.description}</p>
   </div>
   <ul class="list-group list-group-flush">
     <li class="list-group-item">Type: ${data.type}</li>
+    
     <li class="list-group-item">Date: ${data.date.datetime.day} . ${data.date.datetime.month} . ${data.date.datetime.year}</li>
   </ul>
 </div>

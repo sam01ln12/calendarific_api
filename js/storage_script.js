ee57877ceb15137067142307b59afe23908eb348
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-06-04 17:03:41 +0400

    Update storage_script.js

--- js/storage_script.js
+++ js/storage_script.js
@@ -1,19 +1,13 @@
-calendar_storage = (keyword, data)=> { 
-    
-    window.localStorage.setItem(keyword, JSON.stringify(data));
-
-}
-
-
-calendar_get = (keyword) => {
+calendar_get = (keyword, flag_code) => {
     let holidays = localStorage.getItem(keyword);
     let entry = JSON.parse(holidays);
     let countryName = entry[0].country.name;
+    let flag_url = `https://www.worldometers.info/img/flags/${flag_code}-flag.gif`
+    
     let holidaysNum = entry.length;
 
     let searchTable = document.querySelector(".body_of_table");
     let searchIndex = searchTable.childElementCount + 1;
-    let newHoliday = render_data_to_table(searchIndex, countryName, holidaysNum);
+    let newHoliday = render_data_to_table(searchIndex, countryName, holidaysNum, flag_url);
     searchTable.innerHTML += newHoliday;
 }
-
